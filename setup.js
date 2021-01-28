25641f44d8c46591a5f003fad39168b95d9b899e
yusan <mif7748@gmail.com>
2021-01-28 14:23:36 +0800

    新增gitnore，準備修改重傳機制

--- setup.js
+++ setup.js
@@ -1,6 +1,11 @@
 module.exports = {
-    fenceURL :  "https://us-central1-map-test-229313.cloudfunctions.net/fenceSocket",
     picuploadURL : "https://us-central1-utlcamera.cloudfunctions.net/cameraService",
-    serverIP : "140.124.33.80"
+    serverIP : "140.124.33.80",
+    FTPport : "50",
+
+    User:{
+        ID:"UTL",
+        pwd:"utliot2041"
+    }
 }
 
