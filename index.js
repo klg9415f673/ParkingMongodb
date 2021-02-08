2536c7efe26e5c2f95669c599b9f4bd63d5d74b9
yusan <mif7748@gmail.com>
2021-02-08 18:03:18 +0800

    將foramt由6碼改為前四碼，剩餘2碼改為action，目前未啟用，另外將filename設為變數供function使用，並且新增errorlog

--- index.js
+++ index.js
@@ -14,7 +14,26 @@ var tz = require('moment-timezone');
 
 server.bind('8888',IP);
 
-function Delete() {
+function VerifyData(DataArray) {
+	console.log("驗證資料!!!!!")	
+	let WishNumber = 1;
+	let Status = true;
+	let lostlist = []
+	DataArray.forEach(element => { 
+		
+		if(element!=""){
+			let NowNumber = transform.hex2int16(element.substr(0,2))
+			
+			if(NowNumber != WishNumber){lostlist.push(NowNumber);Status = false;}
+			WishNumber++;
+		}
+
+	});
+
+	return {Status:Status,lostlist:lostlist}
+
+}
+function Delete(filename) {
 	fs.unlink(filename, function(err) {
 		if (err) {
 			return console.error('Delete',err);
@@ -23,7 +42,7 @@ function Delete() {
 		});
 }
 
-function ImageData() {
+function ImageData(filename) {
 	
 	fs.readFile(filename, function (err, cameradata) {
 		if (err) {
@@ -32,7 +51,7 @@ function ImageData() {
         console.log("ImageData(MongoDB) start");
 		Imageservice.createService(cameradata.toString('hex'))
 		setTimeout(() => {
-			Delete();
+			Delete(filename);
 		}, 5000);
 
 	});	
@@ -50,7 +69,7 @@ function Devicedata(Device) {
 
 
 
-function UploadImage() {
+function UploadImage(filename) {
 	fs.readFile(filename, function (err, cameradata) {
 		if (err) {
 			return console.error('readFile',err);
@@ -88,18 +107,21 @@ server.on('message', async function (message, remote) {
 	// console.log(transform.hex2string(message.toString('hex')))  //PC client測試用
 	// message = transform.hex2string(message.toString('hex'))	   //PC client測試用
 
-	var format = message.toString('hex').substr(0,6)
+	var format = message.toString('hex').substr(0,4)
 	if (message.toString('hex') =='636c69656e74'){//client ASCII
 		format = String(message)
 	}
 
+	//創建txt &寫入資料
+	var filename = './RawData/'+ remote.address +'-'+ remote.port +'.txt' ;
 
 	switch(format){
-		case "eeeeee":
+		case "eeee":
+			
 			if(message.toString('hex').indexOf('ffd8')!=-1){
-				fs.exists(filename, function(exists) {
-					if(exists){
-						Delete();
+				fs.access(filename, function(exists) {
+					if(!exists){
+						Delete(filename);
 						
 					}
 				});
@@ -107,51 +129,79 @@ server.on('message', async function (message, remote) {
 			console.log(message.toString('hex'));
 			console.log("recognize ",format)
 
-			//創建txt &寫入資料
-			filename = './RawData/'+ remote.address +'-'+ remote.port +'.txt' ;
+			
 
 			fs.appendFile(filename, message,function(error){
 				if(error){
 					console.log('appendFile',error);
 				}else{
 					console.log('Write image data successful');
-					if(message.toString('hex').indexOf('ffd9')!=-1){
-						fs.readFile(filename, function (err, cameradata){
-							// UploadImage();
-							// ImageData();
-							let cameraData = String(cameradata.toString('hex'))
-							let DataArr = cameraData.split(cameraData.substr(0,24));
-							let WishNumber = 1;
-							let Status = true;
-							console.log("驗證資料!!!!!")
-							DataArr.forEach(element => { 
-								
-    							if(element!=""){
-									let NowNumber = transform.hex2int16(element.substr(0,2))
-									
-									if(NowNumber != WishNumber){Status = false;}
-			
-									WishNumber++;
+					action = message.toString('hex').substr(4,2);
+					switch(action){
+						case "00":
+							fs.readFile(filename, function (err, cameradata) {
+								if (err) {
+									return console.error('readFile',err);
 								}
-		
+								Data = cameradata.toString('hex')
+								var DataArr = Data.split(Data.substr(0,24));
+								var newarr =[]
+								
+								DataArr.forEach(element => { 
+									if(element!=""){
+										
+										let msg = Buffer.from(transform.stringToHex(Data.substr(0,24) + element))
+										let NowNumber = transform.hex2int16(element.substr(0,2))
+										newarr.splice(NowNumber-1, 0, (msg.toString('hex')));
+									}
+							
+								});
+								
+								newdata = newarr.join('')
+								fs.writeFile(filename,newdata)
 							});
-
-							switch(Status){	
-								case false :		
-									server.send('resend',remote.port,remote.address);
-									console.log("Some Data MISSING!!!!!!!!!!!!");
-									Delete();
-									break;
-								case true :
-									server.send('oksend',remote.port,remote.address);
-									UploadImage();
-									ImageData();
-									break;
+							verifydata= VerifyData(DataArr);
+							Status = verifydata.Status
+							lostlist = verifydata.lostlist
+							if(Status === true){
+								server.send('okresend',remote.port,remote.address);
+								UploadImage(filename);
+								ImageData(filename);
 							}
-						
-						})
+						break;
+						case "ee":
+							if(message.toString('hex').indexOf('ffd9')!=-1){
+								fs.readFile(filename, function (err, cameradata){
+									cameraData = String(cameradata.toString('hex'))
+									DataArr = cameraData.split(cameraData.substr(0,24));
+									var verifydata= VerifyData(DataArr);
+							
+		
+									switch( verifydata.Status){	
+										case false :		
+											//server.send(String(verifydata.lostlist),remote.port,remote.address);
+											server.send('resend',remote.port,remote.address);//若要啟用回傳特定筆則需註解
+											console.log("Some Data MISSING!!!!!!!!!!!!");
+											errormsg = `MAC:${cameraData.substr(6,12)}\r\nTIME:${moment().tz("Asia/Taipei").format("MM/DD HH:mm:ss")}\r\nlostlist:${String(verifydata.lostlist)}\r\n`
+											fs.appendFile('error_log', errormsg,function(error){
+												if(err) throw err ;
+											})
+											Delete(filename);//若要啟用回傳特定筆則需註解
 
+											break;
+										case true :
+											server.send('oksend',remote.port,remote.address);
+											UploadImage(filename);
+											ImageData(filename);
+											break;
+									}
+								
+								})
+		
+							}
+						break;
 					}
+				
 			
 				}
 
@@ -159,7 +209,7 @@ server.on('message', async function (message, remote) {
 
 			break;
 
-		case "ffffff":			
+		case "ffff":			
 			
 			console.log(message.toString('hex'));
 			var msg = Buffer.from('!' + moment().tz("Asia/Taipei").format("MMDDHHmmss"))
